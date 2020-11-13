9a84f1f49cb325435ed5c5b57e4dcfea71b31441
loganfillo <logan.fillo@gmail.com>
2020-11-12 20:11:34 -0800

    Removed default locale

--- src/constants/defaults.js
+++ src/constants/defaults.js
@@ -1,5 +1,5 @@
 import lightTheme from 'constants/themes/light'
-import { i18n } from 'next.config'
+// import { i18n } from 'next.config'
 
-export const DEFAULT_LOCALE =  i18n.defaultLocale
+// export const DEFAULT_LOCALE =  i18n.defaultLocale
 export const DEFAULT_THEME = lightTheme
